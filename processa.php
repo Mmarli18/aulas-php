fcb7321c3df4ed61580ab99c414e316f6eb828b4
Mmarli18 <mahmorais0@gmail.com>
2024-05-22 14:52:58 -0300

    formulario PHP

---
+++ processa.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processando os dados </title>
+</head>
+<body>
+    <h1>Processamento dos dados vindos do formulário</h1>
+    <hr>
+
+    <?php
+/* Capturando os dados provenientes do formulário
+usando o array superglobal $_POST[] se form usar método get, então usaríamos $_GET[]. */
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$nascimento = $_POST['nascimento'];
+$mensagem = $_POST['mensagem'];
+
+// Operador || siginifica OU
+
+if( empty ($nome) || empty($email) ){
+    ?>
+    <p>Campos obrigatórios <b>nome</b> e <b>e-mail!</b></p>
+    <?php
+} else {
+?>
+
+
+<h2>Dados Recebidos</h2>
+<ul>
+    <li>Nome: <?=$nome?> </li>
+    <li>E-mail: <?=$email?> </li>
+    <li>Data de Nascimento: <?=$nascimento?> </li>
+    <li>Mensagem: <?=$mensagem?> </li>
+</ul>
+<?php } ?>
+</body>
+</html>
